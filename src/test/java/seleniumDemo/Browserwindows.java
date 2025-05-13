752050aefd05ee0e065db0faf8203987b6ce6864
aparna.anantharaju <aparna.anantharaju@globallogic.com>
2025-05-13 13:44:46 +0530

    Renamed classes

--- src/test/java/seleniumDemo/Browserwindows.java
+++ src/test/java/seleniumDemo/Browserwindows.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.Set;
-
+public class Browserwindows{
   public static void main(String[] args) throws InterruptedException {
     WebDriverManager.chromedriver().setup();
     WebDriver driver = new ChromeDriver();
